f27df0d46c5184215ecdb08630d91a8a6ff8af50
Norasavira <norasavirasavira@gmail.com>
2021-03-01 23:56:07 +0700

    first commit

---
+++ app/src/main/java/com/example/bankquis/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.bankquis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView kuis;
+    RadioGroup rg;
+    RadioButton PilihanA, PilihanB, PilihanC, PilihanD;
+    int nomor = 0;
+    public static int hasil, benar, salah;
+
+    //pertanyaan
+    String[] pertanyaan_kuis = new String[]{
+            "1. manusia bernapas dengan?",
+            "2. hewan berkaki empat?",
+            "3. kucing berkembang biak dengan cara?",
+            "4. apa itu simbiosis? ",
+    };
+
+    //pilihan jawaban a, b, c, d
+    String[] pilihan_jawaban = new String[]{
+            "pankreas", "paru paru", "insang", "kulit",
+             "kucing", "sapi", "kambing", "semua benar",
+            "bertelur", "membelah diri", "melahirkan", "semua salah",
+            "hubungan erat antar makhluk hidup", "hubungan antara tumbuhan", "hubungan erat teman", "hubungan erat antara tumbuhan",
+    };
+
+    //jawaban benar
+    String[] jawaban_benar = new String[]{
+            "paru paru",
+            "semua benar",
+            "melahirkan",
+            "hubungan erat antar makhluk hidup",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        kuis = (TextView) findViewById(R.id.kuis);
+        rg = (RadioGroup) findViewById(R.id.pilihan);
+        PilihanA = (RadioButton) findViewById(R.id.pilihanA);
+        PilihanB = (RadioButton) findViewById(R.id.pilihanB);
+        PilihanC = (RadioButton) findViewById(R.id.pilihanC);
+        PilihanD = (RadioButton) findViewById(R.id.pilihanD);
+
+        kuis.setText(pertanyaan_kuis[nomor]);
+        PilihanA.setText(pilihan_jawaban[0]);
+        PilihanB.setText(pilihan_jawaban[1]);
+        PilihanC.setText(pilihan_jawaban[2]);
+        PilihanD.setText(pilihan_jawaban[3]);
+
+        rg.check(0);
+        benar = 0;
+        salah = 0;
+    }
+
+    public void next(View view) {
+        if (PilihanA.isChecked() || PilihanB.isChecked() || PilihanC.isChecked() || PilihanD.isChecked()) {
+
+            RadioButton jawaban_user = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
+            String ambil_jawaban_user = jawaban_user.getText().toString();
+            rg.check(0);
+            if (ambil_jawaban_user.equalsIgnoreCase(jawaban_benar[nomor])) benar++;
+            else salah++;
+            nomor++;
+            if (nomor < pertanyaan_kuis.length) {
+                kuis.setText(pertanyaan_kuis[nomor]);
+                PilihanA.setText(pilihan_jawaban[(nomor * 4) + 0]);
+                PilihanB.setText(pilihan_jawaban[(nomor * 4) + 1]);
+                PilihanC.setText(pilihan_jawaban[(nomor * 4) + 2]);
+                PilihanD.setText(pilihan_jawaban[(nomor * 4) + 3]);
+
+            } else {
+                hasil = benar * 20;
+                Intent selesai = new Intent(getApplicationContext(), HasilKuis.class);
+                startActivity(selesai);
+            }
+        }
+        else {
+            Toast.makeText(this,"Kamu Jawab Dulu",Toast.LENGTH_LONG).show();
+        }
+    }
+}
+
